6eceea28c7060ea07043a163a2e711b4dfca44a7
Cao Quốc Huy <caoquochuy2002@yahoo.com>
2025-05-30 22:33:30 +0700

    ✅ Update table :D

--- ocr_capture_app.py
+++ ocr_capture_app.py
@@ -4,4 +4,4 @@ import streamlit.components.v1 as components
 st.set_page_config(page_title="📦 Make Life Easier !!!", layout="centered")
 st.title("📦 Make Life Easier !!!")
 
-components.html(open("index.html", encoding="utf8").read(), height=800)
+components.html(open("index.html", encoding="utf8").read(), height=600)
